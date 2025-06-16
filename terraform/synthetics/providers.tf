2c94622ec716a460e5cdbb1fd6f28aa9410f7bb8
piotr.mejer <piotr.mejer@dynatrace.com>
2025-06-16 13:55:40 +0200

    Synthetic

---
+++ terraform/synthetics/providers.tf
@@ -0,0 +1,6 @@
+provider "grafana" {
+  url             = "https://piotrmejer94.grafana.net/"
+  auth            = var.grafana_service_token
+  sm_url          = "https://synthetic-monitoring-api-gb-south-1.grafana.net"
+  sm_access_token = var.sm_access_token
+}
