edc07d856b5d4670c5e9a0d53cd480719076a77d
piotr.mejer <piotr.mejer@dynatrace.com>
2025-06-16 14:01:17 +0200

    Freq changed

--- terraform/synthetics/main.tf
+++ terraform/synthetics/main.tf
@@ -6,7 +6,7 @@ resource "grafana_synthetic_monitoring_check" "Synthetics_BrowserCheck_login" {
   enabled   = true
   probes    = [data.grafana_synthetic_monitoring_probes.main.probes.London]
   labels    = {}
-  frequency = 300000
+  frequency = 300001
   timeout   = 60000
   settings {
     browser {
